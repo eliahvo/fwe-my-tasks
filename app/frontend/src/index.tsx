5a5c40c29bf895e20771e01611aa268a9453b1a3
Eliah Vogel <eliah.vogel@stud.h-da.de>
2020-11-19 13:30:31 +0100

    basic frontend

---
+++ app/frontend/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById('root');
+const element = <div className="container">Hello World!</div>;
+ReactDOM.render(element, rootElement);
